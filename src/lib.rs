1663279ca515dc752fcdc56c0f532d31e559c847
Stochastic-Batman <turmanidze.lado@kiu.edu.ge>
2026-03-02 14:21:34 +0400

    Project Complete

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,7 @@ struct Worker {
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: Option<mpsc::Sender<Job>>,
 }
 
 
@@ -25,9 +25,18 @@ impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         let thread = thread::spawn(move || { 
             loop {
-                let job = receiver.lock().unwrap().recv().unwrap();
-                println!("Worker {id} got a job; executing...");
-                job();
+                let message = receiver.lock().unwrap().recv();
+
+                match message {
+                    Ok(job) => {
+                        println!("Worker {id} got a job; executing...");
+                        job();
+                    }
+                    Err(_) => {
+                        println!("Worker {id} disconnected; shutting down...");
+                        break;
+                    }
+                }
             }
         });
         
@@ -57,7 +66,7 @@ impl ThreadPool {
             workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
-        Ok(ThreadPool { workers, sender })
+        Ok(ThreadPool { workers, sender: Some(sender) })
     }
 
     pub fn execute<F>(&self, f: F)
@@ -65,6 +74,17 @@ impl ThreadPool {
         F: FnOnce() + Send + 'static,
     {
         let job = Box::new(f);
-        self.sender.send(job).unwrap();
+        self.sender.as_ref().unwrap().send(job).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        drop(self.sender.take());
+
+        for worker in self.workers.drain(..) {
+            println!("Shutting down worker {}", worker.id);
+            worker.thread.join().unwrap();
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,8 @@ fn main() {
       
         pool.execute(|| handle_connection(stream));
     }
+
+    println!("Shutting down...");
 }
 
 fn handle_connection(mut stream: TcpStream) {
